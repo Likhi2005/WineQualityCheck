c0e1b47669fd3498c434cc71829fc10e49573eb1
Likhi2005 <likhisgowda2005@gmail.com>
2025-11-09 02:38:34 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a wine Q package",
+    author="Likhi2005",
+    packages=find_packages(),
+    license="MIT"
+)
+
+# setup(
+#     name="wine_quality",
+#     version="0.0.1",
+#     description="Wine quality prediction pipeline",
+#     author="Likhi2005",
+#     packages=find_packages(),
+#     license="MIT",
+#     entry_points={
+#         "console_scripts": [
+#             "wine-train = src.train_and_evaluate:train_and_evaluate"
+#         ]
+#     },
+# )
+
+
+
+# you_numpy
+#     you_numpy
+#     __init__.py
